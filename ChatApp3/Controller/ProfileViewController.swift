4fa4488131d453855552e26f5f041a4de0c03d69
s-odani <s2020576@s.tsukuba.ac.jp>
2020-07-27 14:50:27 +0900

    プロフィール画面のpickerviewの作成

--- ChatApp3/Controller/ProfileViewController.swift
+++ ChatApp3/Controller/ProfileViewController.swift
@@ -8,12 +8,62 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
 
+class ProfileViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet var selectYear: UIPickerView!
+    @IBOutlet var selectIndustry: UIPickerView!
+    
+    let dataList = [
+        "メーカー", "商社", "金融", "小売", "IT", "マスコミ・広告", "官公庁・公社"
+    ]
+
+    let dataList2 = [
+        "21卒", "22卒", "23卒", "24卒"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        selectYear.delegate = self
+                selectYear.dataSource = self
+                selectYear.tag = 1
+                selectIndustry.delegate = self
+                selectIndustry.dataSource = self
+                selectYear.tag = 2
 
-        // Do any additional setup after loading the view.
+            }
+
+          // UIPickerViewの列の数
+          func numberOfComponents(in pickerView: UIPickerView) -> Int {
+              return 1
+          }
+
+          // UIPickerViewの行数、リストの数
+    func pickerView( _ pickerView: UIPickerView,
+                          numberOfRowsInComponent component: Int) -> Int {
+            if pickerView.tag == 1{
+                return dataList2.count
+            } else if pickerView.tag == 2{
+                return dataList.count
+            } else {
+                return dataList2.count
+            }
+          }
+
+          // UIPickerViewの最初の表示
+    func pickerView( _ pickerView: UIPickerView,
+                          titleForRow row: Int,
+                          forComponent component: Int) -> String? {
+              if pickerView.tag == 1{
+                  return dataList2[row]
+              } else if pickerView.tag == 2{
+                  return dataList[row]
+              } else {
+                  return dataList2[row]
+              }
+
+          }
     }
     
 
@@ -27,4 +77,4 @@ class ProfileViewController: UIViewController {
     }
     */
 
-}
+
